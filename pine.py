f67736edb3a151afdc82ec060eb5d76daddb6780
bhoominelwade <142621759+bhoominelwade@users.noreply.github.com>
2024-09-05 00:46:08 +0530

    Update pine.py

--- pine.py
+++ pine.py
@@ -6,16 +6,18 @@ from pinecone import Pinecone
 import numpy as np
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain.vectorstores import Pinecone
-from langchain.vectorstores.base import VectorStore
+from langchain_pinecone import PineconeVectorStore
+from langchain.docstore.document import Document
 
 api_key = os.environ.get("2c584c12-165e-4c19-820e-719e5a9fac4d")
 index_host = os.environ.get("https://boty-zwdzzhc.svc.aped-4627-b74a.pinecone.io")
 
-pc = Pinecone("2c584c12-165e-4c19-820e-719e5a9fac4d")
-index = pc.Index("boty","https://boty-zwdzzhc.svc.aped-4627-b74a.pinecone.io")
+os.environ['PINECONE_API_KEY'] = '2c584c12-165e-4c19-820e-719e5a9fac4d'
 
-index_name="boty"
+# pc = Pinecone("2c584c12-165e-4c19-820e-719e5a9fac4d")
+# index = pc.Index("boty","https://boty-zwdzzhc.svc.aped-4627-b74a.pinecone.io")
+
+index_name="boty2"
 model = "multilingual-e5-large"
 
 # def generate_random_vector(dim=384):
@@ -48,4 +50,13 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1200, chunk_overlap=50
 texts = text_splitter.split_text( pdf_text)
 print(texts[0])
 
-vectorstore=inecone.from_documents(text_key=texts,embedding=embeddings,index_name=index_name)
+documents = [Document(page_content=text) for text in texts]
+
+vectorstore=PineconeVectorStore.from_documents(documents,index_name=index_name,embedding=embeddings)
+
+query="list certifications"
+
+similar_docs=vectorstore.similarity_search(query)
+similar_docs[0]
+
+
